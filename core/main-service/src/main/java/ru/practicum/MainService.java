3ac06ded2e8deb50e2780e8563b0bf30dca25ad4
Ilya Kravchenko <kravchi0803@gmail.com>
2025-09-13 11:17:12 +0300

    change repo visibility

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MainService {
     public static void main(String[] args) {
-        SpringApplication.run(MainService.class,args);
+        SpringApplication.run(MainService.class, args);
     }
 }
